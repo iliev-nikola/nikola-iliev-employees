4561419ce736e68af37f0bb23ce51977bac2aed1
Nikola Iliev <nikola.r.iliev.91@gmail.com>
2021-04-28 21:10:35 +0300

    Create logic for finding project partners

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 let employees;
 
 function upload() {
+    // read the text file and fill employees array with some employees
     const regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
     if (regex.test(FILE_INPUT.value.toLowerCase())) {
         if (typeof (FileReader) != 'undefined') {
@@ -8,7 +9,7 @@ function upload() {
             reader.onload = function (e) {
                 employees = e.target.result.split('\n');
                 employees = employees.slice(0, employees.length - 1);
-                employees.forEach(el => {
+                employees = employees.map(el => {
                     el = el.split(',');
                     const dateFrom = new Date(el[2]).getTime();
                     const dateTo = el[3] === 'NULL\r' ? Date.now() : new Date(el[3]).getTime();
@@ -18,7 +19,8 @@ function upload() {
                         dateFrom: dateFrom,
                         dateTo: dateTo
                     }
-                    console.log(el)
+
+                    return el;
                 });
             }
 
@@ -32,16 +34,45 @@ function upload() {
 
 }
 
-function twoEmployees() {
+function findProjectPartners() {
     const output = [];
 
+    for (let i = 0; i < employees.length - 1; i++) {
+        const emp1 = employees[i];
+        for (let j = i + 1; j < employees.length; j++) {
+            // check if both workers work in same project and on same period
+            const emp2 = employees[j];
+            const ifSameProject = employees[i].projectId === employees[j].projectId;
+            if (!ifSameProject) {
+                continue;
+            }
+            let diff;
+            if (emp2.dateFrom >= emp1.dateFrom && emp2.dateTo <= emp1.dateTo) {
+                diff = emp2.dateTo - emp2.dateFrom;
+            } else if (emp2.dateFrom >= emp1.dateFrom && emp2.dateFrom < emp1.dateTo && emp2.dateTo >= emp1.dateTo) {
+                diff = emp1.dateTo - emp2.dateFrom;
+            } else if (emp2.dateFrom <= emp1.dateFrom && emp2.dateTo > emp1.dateFrom && emp2.dateTo <= emp1.dateTo) {
+                diff = emp2.dateTo - emp1.dateFrom;
+            } else if (emp2.dateFrom <= emp1.dateFrom && emp2.dateTo >= emp1.dateTo) {
+                diff = emp1.dateTo - emp1.dateFrom;
+            }
+
+            if (diff) {
+                const workingDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+                const obj = {
+                    days: workingDays,
+                    emp1: +emp1.empId > +emp2.empId ? +emp2.empId : +emp1.empId,
+                    emp2: +emp1.empId < +emp2.empId ? +emp2.empId : +emp1.empId
+                }
+
+                output.push(obj);
+            }
+        }
+    }
+
+    return output;
 }
 
 
 FILE_INPUT.addEventListener('change', upload);
-BUTTON.addEventListener('click', (e) => {
-    console.log(e.target);
-});
-
-// const diff = dateTo - dateFrom;
-// const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+BUTTON.addEventListener('click', findTwoEmployees);
